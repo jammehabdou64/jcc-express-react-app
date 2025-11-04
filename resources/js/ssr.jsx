392c8bc1428c0733ef431dd4b65eb7b91d41e0fb
jammehabdou64 <jammehabdou64@gmail.com>
2025-11-04 08:54:43 +0000

    update packages

--- resources/js/ssr.jsx
+++ resources/js/ssr.jsx
@@ -1,22 +1,15 @@
 import { createInertiaApp } from "@inertiajs/react";
 import createServer from "@inertiajs/react/server";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import ReactDOMServer from "react-dom/server";
-
-const appName = import.meta.env.VITE_APP_NAME || "JCC";
 
 createServer((page) =>
   createInertiaApp({
     page,
     render: ReactDOMServer.renderToString,
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
-      resolvePageComponent(
-        `./Pages/${name}.tsx`,
-        import.meta.glob("./Pages/**/*.tsx"),
-      ),
-    setup: ({ App, props }) => {
-      return <App {...props} />;
+    resolve: (name) => {
+      const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+      return pages[`./Pages/${name}.jsx`];
     },
+    setup: ({ App, props }) => <App {...props} />,
   }),
 );
