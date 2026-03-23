0de735bd11cbf2a78cae120987e31bcd6dacbbb5
jammehabdou64 <jammehabdou64@gmail.com>
2026-03-23 14:45:51 +0000

    upgrade upgrade packages

--- app/Http/kernel.ts
+++ app/Http/kernel.ts
@@ -1,8 +1,4 @@
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import flash from "connect-flash";
-import fileUpload from "express-fileupload";
-import morgan from "morgan";
+
 import { auth, guest } from "jcc-express-mvc";
 import { inertia } from "jcc-express-mvc/Core/Inertia";
 
@@ -10,14 +6,7 @@ export class Kernel {
   //
 
   public middlewares = [
-    cookieParser(),
-    session({
-      secret: process.env.SESSION_SECRET || "JCCC____",
-      resave: false,
-      saveUninitialized: false,
-    }),
-    flash(),
-    fileUpload(),
+
     inertia({ rootView: "index", ssr: true }),
   ];
 
